aed8a88c608e5d01850863ba0507fd64205e28f8
Ahmed Mardi <a.mardi@75bit.com>
2023-04-04 22:28:11 +0200

    V1.0.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ else:
 
 setuptools.setup(
     name='sleuren',
-    version='1.0.3',
+    version='1.0.4',
     description='Server monitoring agent',
     long_description_content_type='text/markdown',
     long_description=readme,
--- sleuren/sleuren.py
+++ sleuren/sleuren.py
@@ -44,7 +44,7 @@ except ImportError:
     from urllib import urlencode
     from urllib2 import urlopen, Request, HTTPError
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 __FILEABSDIRNAME__ = os.path.dirname(os.path.abspath(__file__))
 
 ini_files = (
